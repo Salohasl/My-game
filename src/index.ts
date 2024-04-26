9b81a5c55b40d755e6daa8352f877ca71539e236
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-04-26 22:09:30 +0200

    upgrade

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import './styles1.scss';
 
 
 type HTMLElementWithClass = HTMLElement & { classList: DOMTokenList };
-// Query the DOM elements
 const block: HTMLElementWithClass | null = document.querySelector('.block');
 const line: any = document.querySelector('.line');
-const btn: HTMLElement | null = document.querySelector('.gameBtn');
+//const btn: HTMLElement | null = document.querySelector('.gameBtn');
+const startBlock: HTMLElement | null = document.querySelector('.start-block');
 const btnStart: any = document.querySelector('.start');
 const num: any = document.querySelector('p');
 const game: any = document.querySelector('.game');
@@ -15,32 +15,41 @@ btnStart?.addEventListener('click', () => {
     setTimeout(() => {
         game.style.display = 'block';
     }, 1000);
-    btnStart.style.display = 'none';
+    startBlock.style.display = 'none';
 });
 
 
-btn?.addEventListener('click', () => {
-    if (num) {
-        num.textContent = (Number(num.textContent) + 1).toString();
-    }
-    if (block && !block.classList.contains('jump')) {
-        block.classList.add('jump');
-    }
+document.addEventListener('keydown', function(event: KeyboardEvent) {
 
-    setTimeout(() => {
-        if (block) {
-            block.classList.remove('jump');
+    if (event.code === 'Space') {
+
+        if (num) {
+            num.textContent = (Number(num.textContent) + 1).toString();
         }
-    }, 800);
+        if (block && !block.classList.contains('jump')) {
+            block.classList.add('jump');
+        }
+    
+        setTimeout(() => {
+            if (block) {
+                block.classList.remove('jump');
+            }
+        }, 800);
+    
+        if (num && Number(num.textContent) >= 5 && Number(num.textContent) <= 10) {
+            document.body.style.backgroundColor = 'darkBlue';
+            line.style.animationDuration = '2s';
+        } else if (num && Number(num.textContent) > 10) {
+            document.body.style.backgroundColor = 'yellow';
+            line.style.animationDuration = '1s';
+        }
 
-    if (num && Number(num.textContent) >= 5 && Number(num.textContent) <= 10) {
-        document.body.style.backgroundColor = 'darkBlue';
-        line.style.animationDuration = '2s';
-    } else if (num && Number(num.textContent) > 10) {
-        document.body.style.backgroundColor = 'yellow';
-        line.style.animationDuration = '1s';
     }
+   
 });
+
+
+
 
 
 const isAlive = setInterval(() => {
@@ -53,7 +62,7 @@ const isAlive = setInterval(() => {
             document.body.style.backgroundColor = 'black';
             line.style.animationDuration = '3s';
             game.style.display = 'none';
-            btnStart.style.display = 'block';
+            startBlock.style.display = 'flex';
         }
     }
 }, 10);
